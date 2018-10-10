566ec4945ce8780a897b382990763f7687ecdb62
eric schkufza <eric.schkufza@gmail.com>
2018-10-10 11:06:00 -0700

    Updating parameters on bitcoin benchmark for de10 debugging.
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- data/benchmark/bitcoin/bitcoin.v
+++ data/benchmark/bitcoin/bitcoin.v
@@ -2,7 +2,7 @@ include sha-256-functions.v;
 include sha256_transform.v;
 include fpgaminer_top.v;
 
-fpgaminer_top#(.LOOP_LOG2(5),.DIFFICULTY(18)) miner(clock.val);
+fpgaminer_top#(.LOOP_LOG2(5),.DIFFICULTY(20)) miner(clock.val);
 
 always @(posedge clock.val) begin
   if (miner.golden_nonce) begin
