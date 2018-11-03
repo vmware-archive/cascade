dd8396e8c73d78062a4b6a3826ace5d680f3515b
eric schkufza <eric.schkufza@gmail.com>
2018-11-02 22:52:23 -0700

    Updated array benchmark to not take 32 MB of state.
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- data/benchmark/array/README.txt
+++ data/benchmark/array/README.txt
@@ -5,14 +5,14 @@ array.v. Larger values of W will produce longer simulations.  The program will
 print out the number of virtual clock cycles required for it to finish. The
 expected number of cycles is given by the formula shown below.
 
-(2^W) * (2^(2*W)-1) + 1 cycles
+(2^W) * (2^(2*W)-1) + 2^W + 1 cycles
 
 W = 2 : 61
     3 : 505
     4 : 4081
-    5 : 32737
-    6 : 262081
-    7 : 2097025
-    8 : 16776961
-    9 : 134217217
-   10 : 1073740801
+    5 : 32737 
+    6 : 262081 
+    7 : 2097153
+    8 : 16777217
+    9 : 134217729
+   10 : 1073741825
--- data/benchmark/array/array.v
+++ data/benchmark/array/array.v
@@ -1,20 +1,17 @@
 localparam W = 10;
 localparam N = (1<<W)-1;
 
-reg[  W-1:0] low;
-reg[2*W-1:0] mid [N:0];    
-reg          high[N:0][N:0];
+reg[W-1:0] low;
+reg[2*W:0] mid [N:0];    
+reg        high[1:0][N:0];
 
 reg[63:0] COUNT = 0;
 always @(posedge clock.val) begin
-  // Every cycle low increases by 1
   low <= low + 1;
-  // Every 2^W cycles mid[N] increases by 1
   mid[low] <= mid[low] + 1; 
-  // It will take 2^W * 2^(2*W) cycles for high[N][N] to go high
-  high[mid[N][2*W-1:W]][mid[N][W-1:0]] <= high[mid[N][2*W-1:W]][mid[N][W-1:0]] + 1;
+  high[mid[N][2*W]][mid[N][W-1:0]] <= high[mid[N][2*W]][mid[N][W-1:0]] + 1;
 
-  if (high[N][N]) begin
+  if (high[1][0]) begin
     $display(COUNT);
     $finish;
   end else
