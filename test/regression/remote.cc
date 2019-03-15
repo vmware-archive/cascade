987ed19b402294ee0bb027d28fa7c32c69f24b39
eric schkufza <eric.schkufza@gmail.com>
2019-03-15 12:57:19 -0700

    Turned off regex remote test for now. Will reenable when file i/o works.
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- test/regression/remote.cc
+++ test/regression/remote.cc
@@ -48,6 +48,7 @@ TEST(remote, bitcoin) {
 TEST(remote, bubble) {
   run_remote("data/test/benchmark/mips32/run_bubble_32.v", "1");
 }
-TEST(remote, regex) {
-  run_remote("data/test/benchmark/regex/run_disjunct_1.v", "424");
-}
+// TODO(eschkufz) This is *really* slow. Reenable this test when file i/o works
+//TEST(remote, regex) {
+//  run_remote("data/test/benchmark/regex/run_disjunct_1.v", "424");
+//}
