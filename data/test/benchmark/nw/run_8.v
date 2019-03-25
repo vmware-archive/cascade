28aee482ff6c2d857e72e240f8e8ce6ba1337f00
eric schkufza <eric.schkufza@gmail.com>
2019-03-25 15:20:16 -0700

    Fixed undefined behavior in nw benchmarks.
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- data/test/benchmark/nw/run_8.v
+++ data/test/benchmark/nw/run_8.v
@@ -24,28 +24,29 @@ Grid#(
   .score(score)
 );
 
-// Read values from the fifo on the clock
 reg done = 0;
 reg once = 0;
+reg[DATA_WIDTH-1:0] buffer = 0;
+reg signed[SWIDTH-1:0] checksum = 0;
+
+// While there are still inputs coming out of the fifo, sum the results
 stream s = $fopen("data/test/benchmark/nw/constants_8.hex");
 always @(posedge clock.val) begin
+  $get(s, buffer);
+  rdata <= buffer;
+
   once <= 1;
-  $get(s, rdata);
   if ($eof(s)) begin
     done <= 1;
   end
-end
 
-// While there are still inputs coming out of the fifo, sum the results
-reg signed[31:0] CHECKSUM = 0;
-always @(posedge clock.val) begin
   // Exit case: print the checksum
   if (done) begin
-    $write(CHECKSUM);
+    $write(checksum);
     $finish(0);
   end
   // Common case: Add score to the checksum
   else if (once) begin
-    CHECKSUM <= CHECKSUM + score;
+    checksum <= checksum + score;
   end
 end
