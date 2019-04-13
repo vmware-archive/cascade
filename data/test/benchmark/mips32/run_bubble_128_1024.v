24e9207cd5a7b55854db43b1b1dd8d992759b4f4
eric schkufza <eric.schkufza@gmail.com>
2019-04-12 17:41:56 -0700

    Removing suspect design pattern from mips benchmarks. Don't want to worry about supporting it just yet.
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- data/test/benchmark/mips32/run_bubble_128_1024.v
+++ data/test/benchmark/mips32/run_bubble_128_1024.v
@@ -1,12 +1,6 @@
 include data/test/benchmark/mips32/mips32.v;
 
-wire[31:0] raddr;
-wire[31:0] instr;
-Mem#(6,32) imem(
-  .clock(clock.val),
-  .raddr1(raddr),
-  .rdata1(instr)
-);
+reg [31:0] imem[63:0];
 
 stream s = $fopen("data/test/benchmark/mips32/run_bubble_128_1024.hex");
 integer i = 0;
@@ -14,11 +8,13 @@ reg[31:0] val = 0;
 initial begin
   for (i = 0; i < 63; i = i + 1) begin
     $get(s, val);
-    imem.mem[i] = val;
+    imem[i] = val;
   end
 end 
 
+wire[31:0] addr;
+wire[31:0] instr = imem[addr];
 Mips32 mips32(
   .instr(instr),
-  .raddr(raddr)
+  .raddr(addr)
 );
