3df6ca3837c4671abcd7c9f34d36607b07a76d82
eric schkufza <eric.schkufza@gmail.com>
2019-10-29 16:22:21 -0700

    Removed some dead files.
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- src/target/core/avmm/fpga/avmm_wrapper.v
+++ src/target/core/avmm/fpga/avmm_wrapper.v
@@ -1,3 +1,6 @@
+`ifndef __AVALON_WRAPPER_V
+`define __AVALON_WRAPPER_V
+
 `include "src/target/core/avmm/fpga/program_logic.v"
 
 reg read = 0;
@@ -7,8 +10,6 @@ reg[31:0] data_in = 0;
 wire[31:0] data_out;
 wire waitreq;
 reg[7:0] temp = 0;
-//integer ifd = STDIN;
-//integer ofd = STDOUT;
 
 program_logic pl (
 	.clk(clock.val),
@@ -23,24 +24,23 @@ program_logic pl (
 
 always @(posedge clock.val) begin
 	if ((read || write) && (!waitreq)) begin
-		if (read) $fwrite(ofd, "%c%c%c%c", data_out[31:24], data_out[23:16], data_out[15:8], data_out[7:0]);
-		//else $fwrite(ofd, "%c", data_in[7:0]);
-		//$fflush(ofd);
-		//if (read) $fwrite(STDERR, "r %h:%h\n", addr, data_out);
-		//else $fwrite(STDERR, "w %h:%h\n", addr, data_in);
+		if (read) 
+      $fwrite(ofd, "%c%c%c%c", data_out[31:24], data_out[23:16], data_out[15:8], data_out[7:0]);
 		read <= 0;
 		write <= 0;
 	end
-	
 	if (!(read || write)) begin
 		$fflush(ifd);
 		$fscanf(ifd, "%c%c%c", temp[7:0], addr[15:8], addr[7:0]);
-		if ($feof(ifd)) begin
+		if ($feof(ifd)) begin 
+      // Does nothing
 		end else begin
-			if (temp[0]) $fscanf(ifd, "%c%c%c%c", data_in[31:24], data_in[23:16], data_in[15:8], data_in[7:0]);
-			//if ($feof(ifd)) $fwrite(STDERR, "error\n");
+			if (temp[0]) 
+        $fscanf(ifd, "%c%c%c%c", data_in[31:24], data_in[23:16], data_in[15:8], data_in[7:0]);
 			read <= temp[1];
 			write <= temp[0];
 		end
 	end
 end
+
+`endif
