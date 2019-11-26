4141906616d58f861cf52e1e7dc4597c25ceda69
eric schkufza <eric.schkufza@gmail.com>
2019-11-25 21:28:17 -0800

    Fixed a bunch of concurrency bugs.
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- src/target/core/avmm/verilator/device/harness.cpp
+++ src/target/core/avmm/verilator/device/harness.cpp
@@ -10,12 +10,15 @@ enum Request {
   READ,
   WRITE,
   WAIT,
-  DONE
+  DONE_1,
+  DONE_2
 };
 
 Vprogram_logic* pl_;
 bool running_;
 Request req_;
+uint16_t addr_;
+uint32_t val_;
 
 } // namespace
 
@@ -30,21 +33,27 @@ extern "C" void verilator_start() {
       case READY:
         break;
       case READ:
+        pl_->s0_address = addr_;
         pl_->s0_read = 1;
         req_ = WAIT;
         break;
       case WRITE:
+        pl_->s0_address = addr_;
+        pl_->s0_writedata = val_;
         pl_->s0_write = 1;
         req_ = WAIT;
         break;
       case WAIT:
         if (!pl_->s0_waitrequest) {
-          req_ = DONE;
+          pl_->s0_read = 0;
+          pl_->s0_write = 0;
+          req_ = DONE_1;
         }
         break;
-      case DONE:
-        pl_->s0_read = 0;
-        pl_->s0_write = 0;
+      case DONE_1:
+        req_ = DONE_2;
+        break;
+      case DONE_2:
         req_ = READY;
         break;
     }
@@ -60,13 +69,13 @@ extern "C" void verilator_stop() {
 }
 
 extern "C" void verilator_write(uint16_t addr, uint32_t val) {
-  pl_->s0_address = addr;
-  pl_->s0_writedata = val;
+  addr_ = addr;
+  val_ = val;
   for (req_ = WRITE; req_ != READY;);
 }
 
 extern "C" uint32_t verilator_read(uint16_t addr) {
-  pl_->s0_address = addr;
+  addr_ = addr;
   for (req_ = READ; req_ != READY;);
   return pl_->s0_readdata;
 }
--- src/target/core/avmm/verilator/verilator_compiler.cc
+++ src/target/core/avmm/verilator/verilator_compiler.cc
@@ -64,28 +64,30 @@ VerilatorLogic* VerilatorCompiler::build(Interface* interface, ModuleDeclaration
 
 bool VerilatorCompiler::compile(const string& text, mutex& lock) {
   (void) lock;
+
+  ofstream ofs(System::src_root() + "/src/target/core/avmm/verilator/device/program_logic.v");
+  ofs << text << endl;
+  ofs.close();
+  System::execute("make -s -C " + System::src_root() + "/src/target/core/avmm/verilator/device");
+
   get_compiler()->schedule_state_safe_interrupt([this, &text]{
-    ofstream ofs(System::src_root() + "/src/target/core/avmm/verilator/device/program_logic.v");
-    ofs << text << endl;
-    ofs.close();
-  
     if (handle_ != nullptr) {
       stop_();
       verilator_.join();
       dlclose(handle_);
     }
     
-    System::execute("make -s -C " + System::src_root() + "/src/target/core/avmm/verilator/device");
     handle_ = dlopen((System::src_root() + "/src/target/core/avmm/verilator/device/obj_dir/libverilator.so").c_str(), RTLD_LAZY);
     stop_ = (void (*)()) dlsym(handle_, "verilator_stop");
     read_ = (uint32_t (*)(uint16_t)) dlsym(handle_, "verilator_read");
     write_ = (void (*)(uint16_t, uint32_t)) dlsym(handle_, "verilator_write");
-
+    
     auto init = (void (*)()) dlsym(handle_, "verilator_init");
     init();
     auto start = (void (*)()) dlsym(handle_, "verilator_start");
     verilator_ = thread(start);
   });
+
   return true;
 }
 
