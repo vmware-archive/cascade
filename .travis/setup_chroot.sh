272327368d620b88a0950bf3f9938f89a8854c6f
eric schkufza <eric.schkufza@gmail.com>
2020-01-06 15:54:44 -0800

    Replaced caching, fixed regular expression match for osx check
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- .travis/setup_chroot.sh
+++ .travis/setup_chroot.sh
@@ -2,34 +2,31 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
+case "$OSTYPE" in
+  "darwin"*)
+    echo "macos doesn't use chroot, no need to mount."
+  *)
+    if [ "$ARCH" = "armhf" ]; then
+      docker run --rm --privileged multiarch/qemu-user-static:register --credential yes
+      cat /proc/sys/fs/binfmt_misc/qemu-arm
+    fi
 
-echo "OS TYPE = "$OSTYPE
+    sudo mount -o bind . $HOME/$ARCH/cascade
+    sudo mount -o bind /dev $HOME/$ARCH/dev
+    sudo mount -o bind /dev/pts $HOME/$ARCH/dev/pts
+    sudo mount -o bind /home $HOME/$ARCH/home
+    sudo mount -o bind /proc $HOME/$ARCH/proc
+    sudo mount -o bind /sys $HOME/$ARCH/sys
 
-if [ "$OSTYPE" = "darwin"* ]; then
-  echo "macos doesn't use chroot, no need to mount."
-else
+    sudo cp $DIR/nopasswd_sudo $HOME/$ARCH/etc/sudoers
+    sudo chown root:root $HOME/$ARCH/etc/sudoers
+    sudo chmod 4755 $HOME/$ARCH/etc/sudoers
+    sudo chown root:root $HOME/$ARCH/usr/bin/sudo
+    sudo chmod 4755 $HOME/$ARCH/usr/bin/sudo
+    sudo chown root:root $HOME/$ARCH/usr/lib/sudo/sudoers.so
+    sudo chmod 4755 $HOME/$ARCH/usr/lib/sudo/sudoers.so
+    sudo chown -R root:root $HOME/$ARCH/etc/sudoers.d
+    sudo chmod -R 4755 $HOME/$ARCH/etc/sudoers.d
 
-  if [ "$ARCH" = "armhf" ]; then
-    docker run --rm --privileged multiarch/qemu-user-static:register --credential yes
-    cat /proc/sys/fs/binfmt_misc/qemu-arm
-  fi
-
-  sudo mount -o bind . $HOME/$ARCH/cascade
-  sudo mount -o bind /dev $HOME/$ARCH/dev
-  sudo mount -o bind /dev/pts $HOME/$ARCH/dev/pts
-  sudo mount -o bind /home $HOME/$ARCH/home
-  sudo mount -o bind /proc $HOME/$ARCH/proc
-  sudo mount -o bind /sys $HOME/$ARCH/sys
-
-  sudo cp $DIR/nopasswd_sudo $HOME/$ARCH/etc/sudoers
-  sudo chown root:root $HOME/$ARCH/etc/sudoers
-  sudo chmod 4755 $HOME/$ARCH/etc/sudoers
-  sudo chown root:root $HOME/$ARCH/usr/bin/sudo
-  sudo chmod 4755 $HOME/$ARCH/usr/bin/sudo
-  sudo chown root:root $HOME/$ARCH/usr/lib/sudo/sudoers.so
-  sudo chmod 4755 $HOME/$ARCH/usr/lib/sudo/sudoers.so
-  sudo chown -R root:root $HOME/$ARCH/etc/sudoers.d
-  sudo chmod -R 4755 $HOME/$ARCH/etc/sudoers.d
-
-  git config --global protocol.version 1
-fi
+    git config --global protocol.version 1
+esac
--- .travis/unmount_env.sh
+++ .travis/unmount_env.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-if [ "$OSTYPE" = "darwin"* ]; then
-  echo "macos doesn't use chroot, no need to unmount."
-else
-  echo "Unmounting chroot mount points..."
-  sudo umount $HOME/$ARCH/cascade
-  sudo umount $HOME/$ARCH/dev/pts
-  sudo umount $HOME/$ARCH/dev
-  sudo umount $HOME/$ARCH/proc
-  sudo umount $HOME/$ARCH/sys
+case "$OSTYPE" in
+  "darwin"*)
+    echo "macos doesn't use chroot, no need to unmount."
+  *)
+    echo "Unmounting chroot mount points..."
+    sudo umount $HOME/$ARCH/cascade
+    sudo umount $HOME/$ARCH/dev/pts
+    sudo umount $HOME/$ARCH/dev
+    sudo umount $HOME/$ARCH/proc
+    sudo umount $HOME/$ARCH/sys
 
-  sudo umount $HOME/$ARCH/home
-fi
+    sudo umount $HOME/$ARCH/home
+esac
